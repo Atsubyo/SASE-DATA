e61279477bf34b0e9211e95ab10bd5f079f1bd20
MikamiV <buninsenshi@gmail.com>
2025-02-21 16:14:15 -0600

    added certain header settings for requests in attendance.ts

--- pages/api/attendance.ts
+++ pages/api/attendance.ts
@@ -91,6 +91,9 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     if (req.method === "GET") {
         const { uin } = req.query;
 
