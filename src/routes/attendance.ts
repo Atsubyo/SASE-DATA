a6b722a055284404049ee0a6c2e4dc32064af01e
Atsubyo <art.tyoung@tamu.edu>
2025-04-21 23:18:06 -0500

    linke hello route

--- src/routes/attendance.ts
+++ src/routes/attendance.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { getAttendance } from '../controllers/attendanceController';
+import { getAttendance, hello } from '../controllers/attendanceController';
 
 const router = Router();
 
+router.get('/hello', hello);
 router.get('/:uin', getAttendance);
 
 export default router;
